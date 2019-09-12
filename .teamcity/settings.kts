cb43e38122d139b273bfbc69fdcacc43134a1870
kseniailina <kse1920@yandex.ru>
2019-09-12 14:47:27 +0300

    sss

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
